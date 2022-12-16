208b3d3193d4ef00ea5e21a0816e8b2f82ab87b6
Vishwajeet singh <vishwajeetsingh262005@gmail.com>
2022-12-16 12:10:45 +0530

    added key navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,30 @@
-import React from 'react';
-import { notes } from './helpers';
-import Octave from './Octave';
+import React, { useEffect, useRef } from "react";
+import { notes } from "./helpers";
+import Octave from "./Octave";
 
 function App() {
+  const keyboardRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    keyboardRef.current && keyboardRef.current.focus();
+  }, []);
+
   const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-    console.log(e.currentTarget.value);
     const audio = new Audio(`sounds/piano_${e.currentTarget.value}.mp3`);
     audio.play();
   };
 
-  return <Octave notes={notes} clickHandler={handleClick} />;
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLDivElement>) => {
+    const key = notes.find((item) => item.key === event.key.toUpperCase());
+    const audio = new Audio(`sounds/piano_${key?.note}.mp3`);
+    audio.play();
+  };
+
+  return (
+    <div tabIndex={0} ref={keyboardRef} onKeyDown={handleKeyDown}>
+      <Octave notes={notes} clickHandler={handleClick} />
+    </div>
+  );
 }
 
 export default App;
--- src/helpers.ts
+++ src/helpers.ts
@@ -4,51 +4,63 @@ export type NoteType = {
 
 export const notes: NoteType[] = [
   {
-    note: 'C',
-    color: 'white'
+    note: "C",
+    color: "white",
+    key: "C",
   },
   {
-    note: 'CSharp',
-    color: 'black'
+    note: "CSharp",
+    color: "black",
+    key: "2",
   },
   {
-    note: 'D',
-    color: 'white'
+    note: "D",
+    color: "white",
+    key: "D",
   },
   {
-    note: 'DSharp',
-    color: 'black'
+    note: "DSharp",
+    color: "black",
+    key: "3",
   },
   {
-    note: 'E',
-    color: 'white'
+    note: "E",
+    color: "white",
+    key: "E",
   },
   {
-    note: 'F',
-    color: 'white'
+    note: "F",
+    color: "white",
+    key: "F",
   },
   {
-    note: 'FSharp',
-    color: 'black'
+    note: "FSharp",
+    color: "black",
+    key: "4",
   },
   {
-    note: 'G',
-    color: 'white'
+    note: "G",
+    color: "white",
+    key: "G",
   },
   {
-    note: 'GSharp',
-    color: 'black'
+    note: "GSharp",
+    color: "black",
+    key: "5",
   },
   {
-    note: 'A',
-    color: 'white'
+    note: "A",
+    color: "white",
+    key: "A",
   },
   {
-    note: 'ASharp',
-    color: 'black'
+    note: "ASharp",
+    color: "black",
+    key: "1",
   },
   {
-    note: 'B',
-    color: 'white'
-  }
+    note: "B",
+    color: "white",
+    key: "B",
+  },
 ];
